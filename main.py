e2e117ee43fb863755b3b3dc7f9e01a77933556c
mistralll <miyakei20011007@gmail.com>
2022-10-18 11:19:44 +0900

    change: use "bot" instead of "client".

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from lib2to3.pgen2 import token
 import os
 from dotenv import load_dotenv
 
@@ -10,43 +11,43 @@ UNITE_TX_ID = int(os.environ['UNITE_TX_ID'])
 UNITE_VC_ID = int(os.environ['UNITE_VC_ID'])
 GENSHIN_TX_ID = int(os.environ['GENSHIN_TX_ID'])
 GENSHIN_VC_ID = int(os.environ['GENSHIN_VC_ID'])
+GUILD_ID = [int(os.environ['GUILD_ID'])]
 
 import discord
 
-#intents = discord.Intents(messages=True, guilds=True, members=True, reactions=True)  #before
 intents=discord.Intents.all() # after
 
 client = discord.Client()
 bot = discord.Bot()
 
-@client.event
+@bot.event
 async def on_message(message):
     if message.content == "ping":
         await message.channel.send("pong")
 
-@client.event
+@bot.event
 async def on_ready():
     print("listening...")
 
-@client.event
+@bot.event
 async def on_voice_state_update(member, before, after):
     if after.channel is not None: # Start検知
         id = after.channel.id
-        ch = client.get_channel(id)
+        ch = bot.get_channel(id)
         cnt = len(ch.voice_states.keys())
 
         if cnt == 1: # 通話が開始された
-            client.dispatch("vc_start",member,after.channel)
+            bot.dispatch("vc_start",member,after.channel)
 
     if before.channel is not None: # End判定
         id = before.channel.id
-        ch = client.get_channel(id)
+        ch = bot.get_channel(id)
         cnt = len(ch.voice_states.keys())
         
         if cnt == 0: # 通話が終了した
-            client.dispatch("vc_end",member,before.channel)
+            bot.dispatch("vc_end",member,before.channel)
 
-@client.event
+@bot.event
 async def on_vc_start(member, channel):
     msg = f"{member.display_name}が{channel.name}に来たようです！"
     print(msg)
@@ -58,9 +59,9 @@ async def on_vc_start(member, channel):
     if str(channel.id) == str(GENSHIN_VC_ID): # 原神部
         chid = GENSHIN_TX_ID
     
-    await client.get_channel(int(chid)).send(msg)
+    await bot.get_channel(int(chid)).send(msg)
 
-@client.event
+@bot.event
 async def on_vc_end(member, channel):
     msg = f"{channel.name}の通話は終了しました。"
     print(msg)
@@ -72,12 +73,6 @@ async def on_vc_end(member, channel):
     if str(channel.id) == str(GENSHIN_VC_ID): # 原神部
         chid = GENSHIN_TX_ID
     
-    await client.get_channel(int(chid)).send(msg)
+    await bot.get_channel(int(chid)).send(msg)
 
-@bot.slash_command(name="engo", guild_ids=[843868374919282718])
-async def engo(ctx, name: str = None):
-    name = name or ctx.author.name
-    print(f"{name}を援護！")
-    await ctx.respond(f"{name}を援護！")
-
-client.run(DISCORD_BOT_TOKEN)
+bot.run(DISCORD_BOT_TOKEN)
