121aea2747a2c37c5a715fca8401ee3a9dd281d9
mistralll <miyakei20011007@gmail.com>
2022-12-28 13:45:16 +0900

    clean: remove debug print

--- random_teaming.py
+++ random_teaming.py
@@ -5,8 +5,6 @@ async def move_random(channel_move_from: discord.VoiceChannel, channels_move_to:
     # 第一引数のVoiceChannelに参加しているメンバーをランダムでmove_toのボイスチャンネルに均等に分配します。
     members = channel_move_from.members
     random.shuffle(members)
-    print("check")
     for i, mem in enumerate(members):
         ch = channels_move_to[i % len(channels_move_to)]
-        print(f"{mem.name} -> {ch.name}")
         await mem.move_to(ch)
