a7143b708bf2f95fe60d534f6b05a9b09aad3227
mistralll <miyakei20011007@gmail.com>
2023-02-06 17:49:19 +0900

    clean: refactoring.

--- disc/vc_notice/voice_state.py
+++ disc/vc_notice/voice_state.py
@@ -3,35 +3,48 @@ sys.path.append('../')
 import disc.bot as bot
 
 import discord
-import disc.vc_notice.funcs as funcs
 import disc.libs.voice_channel as vc
 import disc.random_teaming.funcs as random_teaming
 
+def detect_operation(bf:discord.VoiceState, af:discord.VoiceState):
+    # voice_stateの変化から通話の開始・終了・大人数の参加を検出します。
+    bf_cnt = vc.count_people(bf.channel)
+    af_cnt = vc.count_people(af.channel)
+
+    is_dif_ch = True
+    if bf_cnt != -1 and af_cnt != -1:
+        is_dif_ch = bf.channel.id is af.channel.id
+
+    if af_cnt != -1:
+        if bf_cnt == -1 and af_cnt == 1:
+            return "start"
+        if af_cnt == 1 and not(is_dif_ch):
+            return "start"
+        if af_cnt > 3 and is_dif_ch and bf_cnt < af_cnt:
+            return "many"
+    if bf_cnt == 0:
+        return "end"
+    return -1
+
 @bot.bot.event # 通話検知
 async def on_voice_state_update(member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
-    op = funcs.detect_operation(before, after)
+    op = detect_operation(before, after)
     if op == "start":
-        bot.bot.dispatch("vc_start", member, after.channel)
+        await on_vc_start(member, after.channel)
     if op == "end":
-        bot.bot.dispatch("vc_end", before.channel)
+        await on_vc_end(before.channel)
     if op == "many":
-        bot.bot.dispatch("vc_many", member, after.channel)
+        await on_vc_many(member, after.channel)
 
-@bot.bot.event # 通話開始
+# 通話開始
 async def on_vc_start(mem: discord.Member, ch: discord.channel):
     bot.log(f"VC_Start: {ch.name} is started.")
     emb = discord.Embed(title=f"{ch.name} で通話が開始されました！", description=f"{mem.display_name}")
     chid = vc.detect_ch_id(bot.notice_channels, ch.id)
     await bot.bot.get_channel(int(chid)).send(embed=emb)
 
-@bot.bot.event # 大人数参加
-async def on_vc_many(mem: discord.Member, ch: discord.channel):
-    bot.log(f"VC_Many: {mem.display_name} is join to {ch.name}.")
-    emb = discord.Embed(title=f"{ch.name} に {vc.count_people(ch)}人目の参加者がきました！", description=f"来た人: {mem.display_name}")
-    chid = vc.detect_ch_id(bot.notice_channels, ch.id)
-    await bot.bot.get_channel(int(chid)).send(embed=emb)
 
-@bot.bot.event # 通話終了
+# 通話終了
 async def on_vc_end(ch: discord.channel):
     bot.log(f"VC_End: {ch.name} is ended.")
     emb = discord.Embed(title=f"{ch.name} の通話は終了しました")
@@ -39,3 +52,9 @@ async def on_vc_end(ch: discord.channel):
     await bot.bot.get_channel(int(chid)).send(embed=emb)
     await random_teaming.delete_temp(ch, bot.temp_cat)
 
+# 大人数の参加
+async def on_vc_many(mem: discord.Member, ch: discord.channel):
+    bot.log(f"VC_Many: {mem.display_name} is join to {ch.name}.")
+    emb = discord.Embed(title=f"{ch.name} に {vc.count_people(ch)}人目の参加者がきました！", description=f"来た人: {mem.display_name}")
+    chid = vc.detect_ch_id(bot.notice_channels, ch.id)
+    await bot.bot.get_channel(int(chid)).send(embed=emb)
