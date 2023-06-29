f1211a19e6a59ebb75dc09345a324357285b80fb
mistralll <miyakei20011007@gmail.com>
2023-06-29 22:58:03 +0900

    fix: slash command to command

--- disc/engo_pin/engo_pin.py
+++ disc/engo_pin/engo_pin.py
@@ -5,7 +5,7 @@ import disc.bot as bot
 import discord
 import libs.env as env
 
-@bot.bot.slash_command(guild_ids=[env.GUILD_ID], description="指定のユーザーに援護ピンを立てます。") # 援護ピン
+@bot.bot.command(guild_ids=[env.GUILD_ID], description="指定のユーザーに援護ピンを立てます。") # 援護ピン
 async def engo(ctx, user : discord.User):
     msg = f"{user.mention}を援護！"
     bot.log(f"Slash_Engo: {msg}")
